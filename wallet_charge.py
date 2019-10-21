baea4a81cc84a58a31e3db96dc70543e9696217d
TatchNicolas <TatchNicolas@users.noreply.github.com>
2019-10-21 12:00:33 +0900

    utilをフォルダ下において__init__.py

---
+++ wallet_charge.py
@@ -0,0 +1,55 @@
+import json
+import os
+from datetime import datetime
+
+import boto3
+import requests
+
+
+def wallet_charge(event, context):
+    wallet_table = boto3.resource('dynamodb').Table(os.environ['WALLET_TABLE'])
+    history_table = boto3.resource('dynamodb').Table(os.environ['PAYMENT_HISTORY_TABLE'])
+    body = json.loads(event['body'])
+    result = wallet_table.scan(
+        ScanFilter={
+            'userId': {
+                'AttributeValueList': [
+                    body['userId']
+                ],
+                'ComparisonOperator': 'EQ'
+            }
+        }
+    )
+    user_wallet = result['Items'].pop()
+    total_amount = user_wallet['amount'] + body['chargeAmount']
+    wallet_table.update_item(
+        Key={
+            'id': user_wallet['id']
+        },
+        AttributeUpdates={
+            'amount': {
+                'Value': total_amount,
+                'Action': 'PUT'
+            }
+        }
+    )
+    history_table.put_item(
+        Item={
+            'walletId': user_wallet['id'],
+            'transactionId': body['transactionId'],
+            'chargeAmount': body['chargeAmount'],
+            'locationId': body['locationId'],
+            'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        }
+    )
+    requests.post(os.environ['NOTIFICATION_ENDPOINT'], json={
+        'transactionId': body['transactionId'],
+        'userId': body['userId'],
+        'chargeAmount': body['chargeAmount'],
+        'totalAmount': int(total_amount)
+    })
+
+    return {
+        'statusCode': 202,
+        'body': json.dumps({'result': 'Assepted. Please wait for the notification.'})
+    }
